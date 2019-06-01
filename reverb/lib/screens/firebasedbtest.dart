a6b424228fd98e5ff375cc79ad83582a1ec134f4
Rohit R. Abbadi <rohit@infito.com>
2019-06-01 13:34:26 +0530

    firapp impl test

--- reverb/lib/screens/firebasedbtest.dart
+++ reverb/lib/screens/firebasedbtest.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+
 import 'package:adhara/adhara.dart';
-import 'dart:io' show Platform;
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/material.dart';
 
 class FirebaseDBTest extends AdharaStatefulWidget{
   @override
@@ -11,7 +12,7 @@ class FirebaseDBTest extends AdharaStatefulWidget{
 
 class _FirebaseDBTestState extends AdharaState<FirebaseDBTest>{
 
-  addToDb(){
+  /*addToDb(){
     /*final FirebaseApp app = await FirebaseApp.configure(
       name: 'db2',
       options: Platform.isIOS
@@ -30,21 +31,131 @@ class _FirebaseDBTestState extends AdharaState<FirebaseDBTest>{
       'title': 'Realtime db rocks',
       'created_at': DateTime.now().toIso8601String()
     });
+  }*/
+
+  int _counter;
+  DatabaseReference _counterRef;
+  DatabaseReference _messagesRef;
+  StreamSubscription<Event> _counterSubscription;
+  StreamSubscription<Event> _messagesSubscription;
+  bool _anchorToBottom = false;
+
+  String _kTestKey = 'Hello';
+  String _kTestValue = 'world!';
+  DatabaseError _error;
+
+  @override
+  void initState() {
+    // Demonstrates configuring to the database using a file
+    _counterRef = FirebaseDatabase.instance.reference().child('counter');
+    // Demonstrates configuring the database directly
+    final FirebaseDatabase database = FirebaseDatabase();
+    _messagesRef = database.reference().child('messages');
+    database.reference().child('counter').once().then((DataSnapshot snapshot) {
+      print('Connected to second database and read ${snapshot.value}');
+    });
+    database.setPersistenceEnabled(true);
+    database.setPersistenceCacheSizeBytes(10000000);
+    _counterRef.keepSynced(true);
+    _counterSubscription = _counterRef.onValue.listen((Event event) {
+      setState(() {
+        _error = null;
+        _counter = event.snapshot.value ?? 0;
+      });
+    }, onError: (Object o) {
+      final DatabaseError error = o;
+      setState(() {
+        _error = error;
+      });
+    });
+    _messagesSubscription =
+        _messagesRef.limitToLast(10).onChildAdded.listen((Event event) {
+          print('Child added: ${event.snapshot.value}');
+        }, onError: (Object o) {
+          final DatabaseError error = o;
+          print('Error: ${error.code} ${error.message}');
+        });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _messagesSubscription.cancel();
+    _counterSubscription.cancel();
+  }
+
+  Future<void> _increment() async {
+    // Increment counter in transaction.
+    final TransactionResult transactionResult =
+    await _counterRef.runTransaction((MutableData mutableData) async {
+      mutableData.value = (mutableData.value ?? 0) + 1;
+      return mutableData;
+    });
+
+    if (transactionResult.committed) {
+      _messagesRef.push().set(<String, String>{
+        _kTestKey: '$_kTestValue ${transactionResult.dataSnapshot.value}'
+      });
+    } else {
+      print('Transaction not committed.');
+      if (transactionResult.error != null) {
+        print(transactionResult.error.message);
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Column(
-          children: <Widget>[
-            Text("testing firebase DB"),
-            FlatButton(
-              child: Text("add"),
-              onPressed: addToDb,
-            )
-          ],
-        ),
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: Center(
+              child: _error == null
+                  ? Text(
+                'Button tapped $_counter time${_counter == 1 ? '' : 's'}.\n\n'
+                    'This includes all devices, ever.',
+              )
+                  : Text(
+                'Error retrieving button tap count:\n${_error.message}',
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Checkbox(
+              onChanged: (bool value) {
+                setState(() {
+                  _anchorToBottom = value;
+                });
+              },
+              value: _anchorToBottom,
+            ),
+            title: const Text('Anchor to bottom'),
+          ),
+          Flexible(
+            child: FirebaseAnimatedList(
+              key: ValueKey<bool>(_anchorToBottom),
+              query: _messagesRef,
+              reverse: _anchorToBottom,
+              sort: _anchorToBottom
+                  ? (DataSnapshot a, DataSnapshot b) => b.key.compareTo(a.key)
+                  : null,
+              itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                  Animation<double> animation, int index) {
+                return SizeTransition(
+                  sizeFactor: animation,
+                  child: Text("$index: ${snapshot.value.toString()}"),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _increment,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
